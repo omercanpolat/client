b9387ce1a81e5b0fec18f389097c805c8b2dc53b
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2023-06-12 13:18:52 +0300

    first upload

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+// import Header from "../Header/Header";
+import "../../scss/Main.scss"
+import DataGrid from "./DataGrid";
+import "../../scss/DataGrid.scss";
+
+ 
+function Main() {
+  const [links, setLinks] = useState([]);
+
+  const BASE_URL = "http://127.0.0.1:8000/links/";
+
+  const getLinks = async () => {
+    try {
+      const { data } = await axios(BASE_URL);
+      setLinks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(links);
+  //? componentDidMount
+  useEffect(() => {
+    getLinks();
+  }, []);
+
+  return (
+    <div className="main">
+      {/* <Header links={links} getLinks={getLinks} /> */}
+      <DataGrid links={links} getLinks={getLinks} />
+    </div>
+  );
+}
+export default Main;
